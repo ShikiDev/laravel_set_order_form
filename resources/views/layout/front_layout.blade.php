34c71eb8f487222dcea97eb6d086ef882a75b1e6
Alan Simons <Vovan1295@bk.ru>
2019-09-27 00:16:21 +0300

    puff

--- resources/views/layout/front_layout.blade.php
+++ resources/views/layout/front_layout.blade.php
@@ -19,17 +19,6 @@
     <div class="container d-flex flex-column flex-md-row justify-content-between">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">Order Foods</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Заказы</a>
-                    </li>
-                </ul>
-            </div>
         </nav>
     </div>
 </header>
